71c25b88ac5a7465d4185066d2663240c3cfcf9b
Rick Anderson <rick@byobwebsite.com>
2017-10-04 16:13:18 -0700

    Initial Submittal for Grading

--- README.md
+++ README.md
@@ -1,5 +1,31 @@
 # fswdn-logs-analysis
-Logs Analysis Project - Udacity Full Stack Web Developer Nanodegree
+## Logs Analysis Project - Udacity Full Stack Web Developer Nanodegree
 
-Formatting of output was accomplished by modifying the code suggested here 
-https://stackoverflow.com/questions/10865483/print-results-in-mysql-format-with-python/#answer-20383011
+This program creates 3 reports
+
+**Most Popular Articles**
+
+This is a report of the 3 most popular articles in the database as plain
+text table. It displays the article title and how many views each article has
+received
+
+**Most Popular Authors**
+
+This is a report of the most popular authors by page views as a plain text
+table.  It ranks the authors by page views displaying the author's name and 
+how many page views their articles have relieved
+
+**Dates with greatest number of page view errors**
+
+This is a report of the 10 days that have had the most page view errors. The 
+report displayed in a list format. It displays the date, the error rate as
+a percentage of page views per error and the total error count
+
+##Running the program
+To run the program type `python3 logs.py` in the console from the directory 
+that this repository resides.  It is dependent on the database 
+configuration for the Udacity Full Stack Web Developer Nanodegree course
+
+
+Formatting of tabular reports was accomplished by modifying the code suggested 
+here https://stackoverflow.com/questions/10865483/print-results-in-mysql-format-with-python/#answer-20383011
--- logs.py
+++ logs.py
@@ -7,6 +7,11 @@ from datetime import datetime
 DBNAME = "news"
 
 
+# Generates a report of the 3 most popular articles in the database as plain
+# text table showing the article title and how many views each article has
+# recieved
+
+
 def most_popular_articles_report():
     select_statement = "SELECT title, count(*) AS views " \
                        "FROM articles JOIN log " \
@@ -14,13 +19,17 @@ def most_popular_articles_report():
                        "GROUP BY title ORDER BY views DESC LIMIT 3"
     report_data = get_data(select_statement)
     report_title = '3 Most Popular Articles'
-    widths = [40, 8]
+    widths = [40, 8]  # widths of printed columns
     columns = []
-    for cd in report_data['column_description']:
+    for cd in report_data['column_description']:  # add column names to report
         columns.append(cd[0])
 
     print_tabular_report(widths, columns, report_data['posts'], report_title)
 
+
+# Generates a report of the most popular authors by page views as a plain text
+# table ranking the authors by pageviews showing the author's name and how many
+# page views their articles have revieved
 
 def most_popular_authors_report():
     select_statement = "SELECT name, count(*) AS views FROM authors " \
@@ -30,12 +39,17 @@ def most_popular_authors_report():
                        "ORDER BY views DESC"
     report_data = get_data(select_statement)
     report_title = 'Author Popularity by Article Page Views'
-    widths = [40, 8]
+    widths = [40, 8]  # widths of printed columns
     columns = []
-    for cd in report_data['column_description']:
+    for cd in report_data['column_description']:  # add column names to report
         columns.append(cd[0])
 
     print_tabular_report(widths, columns, report_data['posts'], report_title)
+
+
+# Generates a report of the 10 days that have had the most errors when a page
+# was requested displayed in a list format showing the date, the error rate as
+# a percentage of page views per error and the total error count
 
 
 def dates_with_greatest_errors_report():
@@ -53,10 +67,20 @@ def dates_with_greatest_errors_report():
     print_dates_with_greatest_errors_report(report_data, report_title)
 
 
+# takes the date object from a dictionary or table and formats it into an
+# easily read US format.  It converts the value in the dictionary to the
+# new value and then returns the dictionary
+
+
 def format_date_in_table(table, key):
     for row in table:
         row[key] = datetime.strftime(row[key], '%B %d, %Y')
     return table
+
+
+# Adds an error rate column to the dictionary. It calculates the error rate
+# and then formats it into an easily read format.  Once formatted it adds the
+# new element to the dictionary
 
 
 def add_error_rate(report_data):
@@ -68,6 +92,11 @@ def add_error_rate(report_data):
     return report_data
 
 
+# This is the query statement for the tabular type reports.  It executes the
+# query and then adds it to a dictionary along with the column names.
+# Finally it returns the resultant dictionary
+
+
 def get_data(select_statement):
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
@@ -75,6 +104,11 @@ def get_data(select_statement):
     report_data = {'posts': c.fetchall(), 'column_description': c.description}
     db.close()
     return report_data
+
+
+# This is the query statement for the list type reports.  It executes the
+# query and then converts it to a dictionary of dictionaries where column names
+# beoome keys. Finally it returns the resultant dictionary
 
 
 def get_data_as_dictionary(select_statement):
@@ -87,6 +121,10 @@ def get_data_as_dictionary(select_statement):
     for row in result_set:
         report_data.append(dict(row))
     return report_data
+
+
+# This prints the tabular style report.  It attempts to mimic the command line
+# query printout using plain text.
 
 
 def print_tabular_report(widths, columns, posts, report_title):
@@ -107,6 +145,9 @@ def print_tabular_report(widths, columns, posts, report_title):
     print('')
 
 
+# This prints the list style report.  It displays the results as a sentence.
+
+
 def print_dates_with_greatest_errors_report(report_data, report_title):
     print('')
     print(report_title)
@@ -117,4 +158,6 @@ def print_dates_with_greatest_errors_report(report_data, report_title):
     print('')
 
 
+most_popular_articles_report()
+most_popular_authors_report()
 dates_with_greatest_errors_report()
