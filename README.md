6314ca6bbefe69b66da1fac85df67b50787bcaa1
Rick Anderson <rick@byobwebsite.com>
2017-10-05 12:10:17 -0700

    Response to Grader's Comments

--- README.md
+++ README.md
@@ -21,11 +21,28 @@ This is a report of the 10 days that have had the most page view errors. The
 report displayed in a list format. It displays the date, the error rate as
 a percentage of page views per error and the total error count
 
+## Setup
+This setup describes the use of the program with Vagrant.  Dependencies include
+Python 3.6, PostgreSQL, psycopg2 and datetime
+
+The vagrant machine and basic setup can be found here - https://github.com/udacity/fullstack-nanodegree-vm
+
+Install the vagrant machine and `vagrant up`
+
+The postgreSQL data can be found here https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip
+
+Unzip the file and place its contents in the virtual machine's `vagrant/` 
+directory.  To import the data into the database, SSH into the `vagrant/`
+directory and run `psql -d news -f newsdata.sql`.  This produces the database
+called "news".
+
+Clone this repository into the `vagrant/` directory.
+
+
 ## Running the program
 
-To run the program type `python3 logs.py` in the console from the directory 
-that this repository resides.  It is dependent on the database 
-configuration for the Udacity Full Stack Web Developer Nanodegree course
+To run the program SSH into the `fswdn-logs-analysis/` directory and type
+ `python3 logs.py` in the console 
 
 
 Formatting of tabular reports was accomplished by modifying the code suggested 
--- logs.py
+++ logs.py
@@ -52,7 +52,7 @@ def most_popular_authors_report():
 # a percentage of page views per error and the total error count
 
 
-def dates_with_greatest_errors_report():
+def dates_with_errors_over_one_percent_report():
     select_statement = "SELECT time::DATE AS date, count(*) FILTER(" \
                        "WHERE status != '200 OK') AS errors, " \
                        "count(*) AS views " \
@@ -62,9 +62,9 @@ def dates_with_greatest_errors_report():
     raw_data = get_data_as_dictionary(select_statement)
     report_data = format_date_in_table(raw_data, 'date')
     report_data = add_error_rate(report_data)
-    report_title = 'Dates with greatest number of page view errors'
+    report_title = 'Dates with page view errors in excess of 1%'
 
-    print_dates_with_greatest_errors_report(report_data, report_title)
+    print_dates_with_errors_over_one_percent_report(report_data, report_title)
 
 
 # takes the date object from a dictionary or table and formats it into an
@@ -79,15 +79,19 @@ def format_date_in_table(table, key):
 
 
 # Adds an error rate column to the dictionary. It calculates the error rate
-# and then formats it into an easily read format.  Once formatted it adds the
-# new element to the dictionary
+# and then formats it into an easily read format.  If the error rate is greater
+# than 1% the data is formatted. Once formatted it adds the
+# new element to a new dictionary
 
 
-def add_error_rate(report_data):
-    for row in report_data:
+def add_error_rate(unformatted_report_data):
+    report_data = []
+    for row in unformatted_report_data:
         errors = row['errors']
         views = row['views']
-        row['error_rate'] = '{0:.2%}'.format(float(errors) / float(views))
+        if float(errors) / float(views) >= 0.01:
+            row['error_rate'] = '{0:.2%}'.format(float(errors) / float(views))
+            report_data.append(dict(row))
 
     return report_data
 
@@ -148,7 +152,7 @@ def print_tabular_report(widths, columns, posts, report_title):
 # This prints the list style report.  It displays the results as a sentence.
 
 
-def print_dates_with_greatest_errors_report(report_data, report_title):
+def print_dates_with_errors_over_one_percent_report(report_data, report_title):
     print('')
     print(report_title)
     print('')
@@ -157,7 +161,7 @@ def print_dates_with_greatest_errors_report(report_data, report_title):
               format(row['date'], row['error_rate'], row['errors']))
     print('')
 
-
-most_popular_articles_report()
-most_popular_authors_report()
-dates_with_greatest_errors_report()
+if __name__ == "__main__":
+    most_popular_articles_report()
+    most_popular_authors_report()
+    dates_with_errors_over_one_percent_report()
